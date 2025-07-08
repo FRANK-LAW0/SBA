57e2b716c2b406a9622b573698e75fb4abd1a5ba
FRANK-LAW0 <166393610+FRANK-LAW0@users.noreply.github.com>
2025-07-08 09:55:09 +0800

    app add_event updated

--- app.py
+++ app.py
@@ -9,14 +9,38 @@ app = Flask(__name__, template_folder='build', static_folder='build/static')
 def init_db():
     conn = sqlite3.connect('sports_day.db')
     cursor = conn.cursor()
+    
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS results (
             result_id INTEGER PRIMARY KEY AUTOINCREMENT,
             athlete_id TEXT NOT NULL,
-            event_name TEXT NOT NULL,
-            result REAL NOT NULL
+            event_id TEXT NOT NULL,
+            result REAL NOT NULL,
+            foreign key (athlete_id) references Athletes(athlete_id),
+            foreign key (event_id) references Events(event_id)
         )
     ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Events (
+            event_id text PRIMARY KEY,
+            event_name TEXT NOT NULL,
+            sex text NOT NULL,
+            grade text not null,
+            check (sex in ('M', 'F') and grade in ('A', 'B', 'C'))
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Athletes (
+            athlete_id TEXT primary key,
+            house text not null,
+            sex text not null,
+            grade text not null,
+            check (sex in ('M', 'F') and grade in ('A', 'B', 'C'))
+        )
+    ''')
+    
     conn.commit()
     conn.close()
 
@@ -35,15 +59,15 @@ def index():
 def add_result():
     if request.method == 'POST':
         athlete_id = request.form['athlete_id']
-        event_name = request.form['event_name']
+        event_id = request.form['event_id']
         result = float(request.form['result'])
         
         conn = sqlite3.connect('sports_day.db')
         cursor = conn.cursor()
         cursor.execute('''
-            INSERT INTO results (athlete_id, event_name, result)
+            INSERT INTO results (athlete_id, event_id, result)
             VALUES (?, ?, ?)
-        ''', (athlete_id, event_name, result))
+        ''', (athlete_id, event_id, result))
         conn.commit()
         conn.close()
         
